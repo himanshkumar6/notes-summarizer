5455e366a2ead11a257dcd15e4ab357b5d63150d
himanshkumar6 <githubhimanshu0@gmail.com>
2025-01-18 18:37:23 +0530

    Update authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,8 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "https://notes-summarizer-w4ex.onrender.com";
-const AI_URL = "https://notes-summarizer-w4ex.onrender.com";
+const API_URL = "http://localhost:3000/api/auth";
 
 axios.defaults.withCredentials = true;
 
